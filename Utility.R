49814787f0d5e88eb25d934b096528073a55394a
TomSmithCGAT <tss38@cam.ac.uk>
2018-09-12 09:47:06 +0100

    adds function to parse PTM output from PD

--- Utility.R
+++ Utility.R
@@ -15,6 +15,101 @@ my_theme <- theme_bw() + theme(
 
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
+
+
+### parsePTMScores ###
+# Function to parse the PD PTM score probabilities column
+# and add columns for downstream filtering
+# obj = data.frame with PD output
+# threshold = score threshold
+# ptm_col = colname for PTM scores
+# prob_split = regex to split PTM probabilities
+# collapse_delimiter = delimiter between multiple values in output columns
+# verbose = print log of events
+
+# to do:
+# add logging?
+
+parsePTMScores <- function(obj, threshold=95, ptm_col="PhosphoRS..Best.Site.Probabilities",
+                           prob_split='; |: ', collapse_delimiter=";", verbose=TRUE){
+  
+  if(class(obj)!="data.frame"){
+    stop("first argument must be a data.frame")
+  }
+  
+  # initiate vectors with empty string
+  # where the PTM(s) pass the threshold, these will be updated
+  filtered_ptm_desc <- filtered_ptm_res <- filtered_ptm_pos <- filtered_ptm <- filtered_ptm_score <- rep("", nrow(df))
+  
+  split_probabities <- strsplit(obj[[ptm_col]], split=prob_split)
+  
+  log <- list("Total peptides"=0, "Retained peptides"=0, "Filtered peptides"=0,
+              "Total sites"=0, "Retained sites"=0, "Filtered sites"=0,
+              "Too many isoforms"=0)
+  
+  
+  for (i in seq_along(split_probabities)){
+    
+    peptide_ptm_scores <- split_probabities[[i]]
+    
+    log$`Total peptides` <- log$`Total peptides` + 1
+    
+    if (peptide_ptm_scores[[1]] == "Too many isoforms"){
+      log$`Too many isoforms` <- log$`Too many isoforms` + 1
+      log$`Filtered peptides` <- log$`Filtered peptides` + 1
+      next()
+    }
+
+    log$`Total sites` <- log$`Total sites` + length(peptide_ptm_scores)/2
+    
+    scores <- peptide_ptm_scores[seq(2, length(peptide_ptm_scores), 2)]
+    
+    # if any score is below threshold, disregard all putative ptm sites
+    if (any(as.numeric(scores)<threshold)){
+      log$`Filtered sites` <- log$`Filtered sites` + length(peptide_ptm_scores)/2
+      log$`Filtered peptides` <- log$`Filtered peptides` + 1
+      # if we want to handle this differently, can implement an alternative approach here
+      # and move the rest of the code below into an else clause
+      next()
+    }
+    
+    log$`Retained sites` <- log$`Retained sites` + length(peptide_ptm_scores)/2
+    log$`Retained peptides` <- log$`Retained peptides` + 1
+    
+    ptms <- peptide_ptm_scores[seq(1, length(peptide_ptm_scores), 2)] # extract the PTMs info
+    split_ptms <- unlist(strsplit(ptms, split =  '\\(|\\)')) # split to remove parantheses
+    modifications <- split_ptms[seq(2, length(split_ptms), 2)] # extract modifications, e.g "phospho"
+    positions <- split_ptms[seq(1, length(split_ptms), 2)] # extract the positions, e.g "S6"
+    residues <- substr(positions, 1, 1) # extract first element, e.g S
+    positions <- sub('.', '', positions) # remove first element and leave position, e.g 6
+    
+    # paste together the value, separated by option(collapse_delimiter) and update vectors which will become columns
+    filtered_ptm_desc[[i]] <- paste(peptide_ptm_scores, collapse=collapse_delimiter)
+    filtered_ptm_res[[i]] <- paste(residues, collapse=collapse_delimiter)
+    filtered_ptm_pos[[i]] <- paste(positions, collapse=collapse_delimiter)
+    filtered_ptm[[i]] <- paste(modifications, collapse=collapse_delimiter)
+    filtered_ptm_score[[i]] <- paste(scores, collapse=collapse_delimiter)
+    
+  }
+  
+  # add columns
+  obj['filtered_PTM_desc'] = filtered_ptm_desc
+  obj['filtered_res'] = filtered_ptm_res
+  obj['filtered_pos'] = filtered_ptm_pos
+  obj['filtered_ptm'] = filtered_ptm
+  obj['filtered_score'] = filtered_ptm_score
+  
+  if(verbose){
+    for(event in names(log)){
+      print(sprintf("%s: %i", event, log[[event]]))
+    }
+  }
+  
+  return(obj)
+}
+
+
+
 makeVennPlot <- function(set1, set2, set3,
                          set1_name="Identified proteins",
                          set2_name="RBPs",
