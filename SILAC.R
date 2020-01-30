75ea376de1f2f8c7b5c812607c4e5a5e8785e4f7
Tom Smith <thomas.smith2@dpag.ox.ac.uk>
2020-01-30 09:20:06 +0000

    updates missing ratio function to allow log base definition

--- SILAC.R
+++ SILAC.R
@@ -30,7 +30,7 @@ my_aggregator <- function(values, func=mean){
 }
 
 
-getMissingRatio <- function(treatment, control, treatment_name, control_name){
+getMissingRatio <- function(treatment, control, treatment_name, control_name, log_base=10){
   
   if (is.na(control)|is.na(treatment)){
     Ratio = NA
@@ -40,17 +40,17 @@ getMissingRatio <- function(treatment, control, treatment_name, control_name){
     }
     else if (is.na(control)){
       class = sprintf("%s Missing", control_name)
-      pseudo_Ratio = log10((2^treatment) + 1)
+      pseudo_Ratio = log((2^treatment) + 1, log_base)
     }
     else {
       class = sprintf("%s Missing", treatment_name)
-      pseudo_Ratio = log10(1/((2^control) + 1))
+      pseudo_Ratio = log(1/((2^control) + 1), log_base)
     }
     
   }
   else{
-    Ratio = log10(((2^treatment)+1) / ((2^control) + 1))
-    pseudo_Ratio = log10(((2^treatment)+1) / ((2^control) + 1))
+    Ratio = log(((2^treatment)+1) / ((2^control) + 1), log_base)
+    pseudo_Ratio = log(((2^treatment)+1) / ((2^control) + 1), log_base)
     class = "Neither Missing"
   }
   return(c(Ratio, pseudo_Ratio, class))
